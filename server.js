a13ff7422ac0832042985bf3a1f1fdc122977311
Ismael Muñoz <Prosilver05@gmail.com>
2021-06-04 18:52:28 -0500

    added server base html and 1 testing img

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-const express = require("epxress")
+const express = require("express")
+const app = express()
+let port = process.env.PORT || 4000
+app.use(express.json())
+
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html")
+})
+
+app.get("/Imagen1", (req,res) => {
+
+  res.sendFile(__dirname + "/resources/testingCat.jpeg")
+
+})
+
+app.listen(port, () => console.log(`App server initalized on port ${port}`))
+
