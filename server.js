bffe115dc5653a0b19a54e81568ec41cd6f076e1
Ismael Muñoz <Prosilver05@gmail.com>
2021-06-09 18:33:31 -0500

    made the code more efficient with a for loop

--- server.js
+++ server.js
@@ -3,311 +3,325 @@ const app = express()
 let port = process.env.PORT || 4000
 app.use(express.json())
 
+const missingList = [12, 23, 34, 45, 56]
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html")
 })
 
-app.get("/Diapositiva2", (req,res) => {
 
-  res.sendFile(__dirname + "/resources/Diapositiva2.jpeg")
+for (let c = 2; c <= 66; c++){
+  if (missingList.includes(c)){} 
+  else {
+    app.get(`/Diapositiva${c}`, (req,res) => {
 
-})
-app.get("/Diapositiva3", (req,res) => {
+      res.sendFile(__dirname + `/resources/Diapositiva${c}.jpeg`)
 
-  res.sendFile(__dirname + "/resources/Diapositiva3.jpeg")
+    })
+  }
+}
 
-})
-app.get("/Diapositiva4", (req,res) => {
 
-  res.sendFile(__dirname + "/resources/Diapositiva4.jpeg")
+// app.get("/Diapositiva2", (req,res) => {
 
-})
-app.get("/Diapositiva5", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva2.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva5.jpeg")
+// })
+// app.get("/Diapositiva3", (req,res) => {
 
-})
-app.get("/Diapositiva6", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva3.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva6.jpeg")
+// })
+// app.get("/Diapositiva4", (req,res) => {
 
-})
-app.get("/Diapositiva7", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva4.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva7.jpeg")
+// })
+// app.get("/Diapositiva5", (req,res) => {
 
-})
-app.get("/Diapositiva8", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva5.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva8.jpeg")
+// })
+// app.get("/Diapositiva6", (req,res) => {
 
-})
-app.get("/Diapositiva9", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva6.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva9.jpeg")
+// })
+// app.get("/Diapositiva7", (req,res) => {
 
-})
-app.get("/Diapositiva10", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva7.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva10.jpeg")
+// })
+// app.get("/Diapositiva8", (req,res) => {
 
-})
-app.get("/Diapositiva11", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva8.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva11.jpeg")
+// })
+// app.get("/Diapositiva9", (req,res) => {
 
-})
-app.get("/Diapositiva13", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva9.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva13.jpeg")
+// })
+// app.get("/Diapositiva10", (req,res) => {
 
-})
-app.get("/Diapositiva14", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva10.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva14.jpeg")
+// })
+// app.get("/Diapositiva11", (req,res) => {
 
-})
-app.get("/Diapositiva15", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva11.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva15.jpeg")
+// })
+// app.get("/Diapositiva13", (req,res) => {
 
-})
-app.get("/Diapositiva16", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva13.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva16.jpeg")
+// })
+// app.get("/Diapositiva14", (req,res) => {
 
-})
-app.get("/Diapositiva17", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva14.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva17.jpeg")
+// })
+// app.get("/Diapositiva15", (req,res) => {
 
-})
-app.get("/Diapositiva18", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva15.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva18.jpeg")
+// })
+// app.get("/Diapositiva16", (req,res) => {
 
-})
-app.get("/Diapositiva19", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva16.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva19.jpeg")
+// })
+// app.get("/Diapositiva17", (req,res) => {
 
-})
-app.get("/Diapositiva20", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva17.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva20.jpeg")
+// })
+// app.get("/Diapositiva18", (req,res) => {
 
-})
-app.get("/Diapositiva21", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva18.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva21.jpeg")
+// })
+// app.get("/Diapositiva19", (req,res) => {
 
-})
-app.get("/Diapositiva22", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva19.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva22.jpeg")
+// })
+// app.get("/Diapositiva20", (req,res) => {
 
-})
-app.get("/Diapositiva24", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva20.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva24.jpeg")
+// })
+// app.get("/Diapositiva21", (req,res) => {
 
-})
-app.get("/Diapositiva25", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva21.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva25.jpeg")
+// })
+// app.get("/Diapositiva22", (req,res) => {
 
-})
-app.get("/Diapositiva26", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva22.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva26.jpeg")
+// })
+// app.get("/Diapositiva24", (req,res) => {
 
-})
-app.get("/Diapositiva27", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva24.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva27.jpeg")
+// })
+// app.get("/Diapositiva25", (req,res) => {
 
-})
-app.get("/Diapositiva28", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva25.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva28.jpeg")
+// })
+// app.get("/Diapositiva26", (req,res) => {
 
-})
-app.get("/Diapositiva29", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva26.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva29.jpeg")
+// })
+// app.get("/Diapositiva27", (req,res) => {
 
-})
-app.get("/Diapositiva30", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva27.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva30.jpeg")
+// })
+// app.get("/Diapositiva28", (req,res) => {
 
-})
-app.get("/Diapositiva31", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva28.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva31.jpeg")
+// })
+// app.get("/Diapositiva29", (req,res) => {
 
-})
-app.get("/Diapositiva32", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva29.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva32.jpeg")
+// })
+// app.get("/Diapositiva30", (req,res) => {
 
-})
-app.get("/Diapositiva33", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva30.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva33.jpeg")
+// })
+// app.get("/Diapositiva31", (req,res) => {
 
-})
-app.get("/Diapositiva35", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva31.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva35.jpeg")
+// })
+// app.get("/Diapositiva32", (req,res) => {
 
-})
-app.get("/Diapositiva36", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva32.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva36.jpeg")
+// })
+// app.get("/Diapositiva33", (req,res) => {
 
-})
-app.get("/Diapositiva37", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva33.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva37.jpeg")
+// })
+// app.get("/Diapositiva35", (req,res) => {
 
-})
-app.get("/Diapositiva38", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva35.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva38.jpeg")
+// })
+// app.get("/Diapositiva36", (req,res) => {
 
-})
-app.get("/Diapositiva39", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva36.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva39.jpeg")
+// })
+// app.get("/Diapositiva37", (req,res) => {
 
-})
-app.get("/Diapositiva40", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva37.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva40.jpeg")
+// })
+// app.get("/Diapositiva38", (req,res) => {
 
-})
-app.get("/Diapositiva41", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva38.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva41.jpeg")
+// })
+// app.get("/Diapositiva39", (req,res) => {
 
-})
-app.get("/Diapositiva42", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva39.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva42.jpeg")
+// })
+// app.get("/Diapositiva40", (req,res) => {
 
-})
-app.get("/Diapositiva43", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva40.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva43.jpeg")
+// })
+// app.get("/Diapositiva41", (req,res) => {
 
-})
-app.get("/Diapositiva44", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva41.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva44.jpeg")
+// })
+// app.get("/Diapositiva42", (req,res) => {
 
-})
-app.get("/Diapositiva46", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva42.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva46.jpeg")
+// })
+// app.get("/Diapositiva43", (req,res) => {
 
-})
-app.get("/Diapositiva47", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva43.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva47.jpeg")
+// })
+// app.get("/Diapositiva44", (req,res) => {
 
-})
-app.get("/Diapositiva48", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva44.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva48.jpeg")
+// })
+// app.get("/Diapositiva46", (req,res) => {
 
-})
-app.get("/Diapositiva49", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva46.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva49.jpeg")
+// })
+// app.get("/Diapositiva47", (req,res) => {
 
-})
-app.get("/Diapositiva50", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva47.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva50.jpeg")
+// })
+// app.get("/Diapositiva48", (req,res) => {
 
-})
-app.get("/Diapositiva51", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva48.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva51.jpeg")
+// })
+// app.get("/Diapositiva49", (req,res) => {
 
-})
-app.get("/Diapositiva52", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva49.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva52.jpeg")
+// })
+// app.get("/Diapositiva50", (req,res) => {
 
-})
-app.get("/Diapositiva53", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva50.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva53.jpeg")
+// })
+// app.get("/Diapositiva51", (req,res) => {
 
-})
-app.get("/Diapositiva54", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva51.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva54.jpeg")
+// })
+// app.get("/Diapositiva52", (req,res) => {
 
-})
-app.get("/Diapositiva55", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva52.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva55.jpeg")
+// })
+// app.get("/Diapositiva53", (req,res) => {
 
-})
-app.get("/Diapositiva57", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva53.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva57.jpeg")
+// })
+// app.get("/Diapositiva54", (req,res) => {
 
-})
-app.get("/Diapositiva58", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva54.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva58.jpeg")
+// })
+// app.get("/Diapositiva55", (req,res) => {
 
-})
-app.get("/Diapositiva59", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva55.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva59.jpeg")
+// })
+// app.get("/Diapositiva57", (req,res) => {
 
-})
-app.get("/Diapositiva60", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva57.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva60.jpeg")
+// })
+// app.get("/Diapositiva58", (req,res) => {
 
-})
-app.get("/Diapositiva61", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva58.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva61.jpeg")
+// })
+// app.get("/Diapositiva59", (req,res) => {
 
-})
-app.get("/Diapositiva62", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva59.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva62.jpeg")
+// })
+// app.get("/Diapositiva60", (req,res) => {
 
-})
-app.get("/Diapositiva63", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva60.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva63.jpeg")
+// })
+// app.get("/Diapositiva61", (req,res) => {
 
-})
-app.get("/Diapositiva64", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva61.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva64.jpeg")
+// })
+// app.get("/Diapositiva62", (req,res) => {
 
-})
-app.get("/Diapositiva65", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva62.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva65.jpeg")
+// })
+// app.get("/Diapositiva63", (req,res) => {
 
-})
-app.get("/Diapositiva66", (req,res) => {
+//   res.sendFile(__dirname + "/resources/Diapositiva63.jpeg")
 
-  res.sendFile(__dirname + "/resources/Diapositiva66.jpeg")
+// })
+// app.get("/Diapositiva64", (req,res) => {
 
-})
+//   res.sendFile(__dirname + "/resources/Diapositiva64.jpeg")
+
+// })
+// app.get("/Diapositiva65", (req,res) => {
+
+//   res.sendFile(__dirname + "/resources/Diapositiva65.jpeg")
+
+// })
+// app.get("/Diapositiva66", (req,res) => {
+
+//   res.sendFile(__dirname + "/resources/Diapositiva66.jpeg")
+
+// })
 
 
 app.listen(port, () => console.log(`App server initalized on port ${port}`))
